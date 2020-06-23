b396432dd59cdd63a18f90050af4f4d25e15f767
mint <officialscientist42@gmail.com>
2020-06-23 04:00:26 -0700

    Adding code outline and documentation for C1M1 Assessment

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-/* Add Author and Project Details here */
+# Array Stats
+
+This project performs simple stats operations on an array
+
+Functions to implement:
+
+- print_statistics()
+- print_array()
+- find_median()
+- find_mean()
+- find_maximum()
+- find_minimum()
+- sort_array()
+
+#Author
+[Ari](https://www.github.com/mint42)
--- stats.h
+++ stats.h
@@ -8,33 +8,102 @@
  * misuse of this material. 
  *
  *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
+
+/*
+** @file stats.h
+** @brief function declarations/notes
+**
+** @author Ari Reedy
+** @date 6/23/2020
+**
+*/
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/*
+** @func print_statistics
+** @brief gathers and prints statistics on a given array
+**
+** calls functions to find median, max, min, and mean values for an array.
+** prints those values.
+** prints array.
+** calls a function to sort the array.
+** prints sorted array.
+**
+** @param data 	array of unsigned char
+** @param len	length of array
+**
+** @return void
+*/
+void			print_statistics(unsigned char *data, unsigned int len);
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
+/*
+** @func print_array
+** @brief prints values of array 
+**
+** @param data 	array of unsigned char
+** @param len	length of array
+**
+** @return void
+*/
+void			print_array(unsigned char *data, unsigned int len);
 
+/*
+** @func find_median
+** @brief find the median value in the array
+**
+** @param data 	array of unsigned char
+** @param len	length of array
+**
+** @return (unsigned char) median value
+*/
+unsigned char	find_median(unsigned char *data, unsigned int len);
+
+/*
+** @func find_mean
+** @brief find the mean value in the array
+**
+** @param data 	array of unsigned char
+** @param len	length of array
+**
+** @return (unsigned char) mean value
+*/
+unsigned char	find_mean(unsigned char *data, unsigned int len);
+
+/*
+** @func find_maximum
+** @brief find the maximum value in the array
+**
+** @param data 	array of unsigned char
+** @param len	length of array
+**
+** @return (unsigned char) maximum value
+*/
+unsigned char	find_maximum(unsigned char *data, unsigned int len);
+
+/*
+** @func find_minimum
+** @brief find the minimum value in the array
+**
+** @param data 	array of unsigned char
+** @param len	length of array
+**
+** @return (unsigned char) minimum value
+*/
+unsigned char	find_minimum(unsigned char *data, unsigned int len);
+
+/*
+** @func sort array
+** @brief sorts the array
+**
+**	performs an insertion sort on the array
+**
+** @param data 	array of unsigned char
+** @param len	length of array
+**
+** @return void
+*/
+void			sort_array(unsigned char *data, unsigned int len);
 
 #endif /* __STATS_H__ */
